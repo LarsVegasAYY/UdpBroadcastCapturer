bfbae09754a6d3494e65f205cb0df017978ec44c
LarsVegasAYY <LarsVegasAYY@users.noreply.github.com>
2020-05-19 19:40:29 +0200

    Add project files.

---
+++ UdpBroadcastCapturer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdpBroadcastCapturer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
